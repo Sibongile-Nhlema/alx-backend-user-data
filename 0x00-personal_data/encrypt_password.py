4dc69323f2fb2d7387bb3163b843b108203e8206
Sibongile <sibongile.nhlema@gmail.com>
2024-07-04 23:06:00 +0200

    Implemented is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,6 +6,17 @@ hashed password, which is a byte string.
 import bcrypt
 
 
+def is_valid(hashed_pwd: bytes, password: str) -> bool:
+    """
+    Args:
+        hashed_pwd (bytes): hashed password to compare with
+        password (str): plaintext password
+    Returns:
+        bool: True matche, otherwise False.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_pwd)
+
+
 def hash_password(password: str) -> bytes:
     '''
     Args:
