c749914b48b92548474533622e76aa9bc8454be5
Sibongile <sibongile.nhlema@gmail.com>
2024-07-04 21:53:12 +0200

    Implemented the format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,23 @@
 Module that handles the filter_datum function
 '''
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -14,7 +30,6 @@ def filter_datum(fields: List[str], redaction: str,
         redaction (str): string of the field will be obfuscated.
         message (str): string of the log line.
         separator (str): string of characters
-
     Returns:
         str: The log message with specified fields obfuscated.
     '''
@@ -30,3 +45,33 @@ def filter_datum(fields: List[str], redaction: str,
     obfuscated_message = re.sub(pattern, replace, message)
 
     return obfuscated_message
+
+
+class RedactingFormatter(logging.Formatter):
+    '''
+    Redacting Formatter class for obfuscating sensitive information in log messages.
+    '''
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        '''
+        Initializes the RedactingFormatter
+        '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+        Returns:
+            str: The formatted log message with sensitive fields obfuscated.
+        '''
+        original_message = super(RedactingFormatter, self).format(record)
+
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        return filtered_message
