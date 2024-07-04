f0997b038aacce227d3ce77a47afb006609f08e7
Sibongile <sibongile.nhlema@gmail.com>
2024-07-04 23:17:50 +0200

    removed the redundant code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -68,21 +68,6 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''
