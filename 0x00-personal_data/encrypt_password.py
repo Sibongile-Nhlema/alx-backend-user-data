12920e1c681a82584a23fdae67359e2b3ccf4959
Sibongile <sibongile.nhlema@gmail.com>
2024-07-04 22:59:51 +0200

    Implemented a hash_password function

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+function that expects one string argument name password and returns a salted,
+hashed password, which is a byte string.
+'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''
+    Args:
+        password (str): password to hash.
+    Returns:
+        bytes: hashed password as bytes.
+    '''
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
