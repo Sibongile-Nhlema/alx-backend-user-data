15df07735d20c2900f7920ad7077ba7aca1263d4
Sibongile <sibongile.nhlema@gmail.com>
2024-07-10 00:42:35 +0200

    Implemented the basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ class BasicAuth(Auth):
     '''
     Class for the BasicAuth implementation
     '''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''
+        function that returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        '''
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        base64_part = authorization_header.split(" ")[1].strip()
+
+        return base64_part
