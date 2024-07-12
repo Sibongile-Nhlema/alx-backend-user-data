ad96abfbe096ae1b5b5255b7424ed11c3c8e8107
Sibongile <sibongile.nhlema@gmail.com>
2024-07-12 20:30:16 +0200

    fixed pycodestyle errors

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,12 +9,14 @@ class BasicAuth(Auth):
     '''
     Class for the BasicAuth implementation
     '''
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         '''
         function that returns the Base64 part of the Authorization
         header for a Basic Authentication
         '''
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
             return None
 
         if not authorization_header.startswith("Basic "):
