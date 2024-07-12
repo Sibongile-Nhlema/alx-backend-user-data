51e3b73e5a479f4d3c512dca1be5b4bfd5ac0d3e
Sibongile <sibongile.nhlema@gmail.com>
2024-07-12 20:41:47 +0200

    implemented decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module for the implementaion of Basic Auth
 '''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,19 @@ class BasicAuth(Auth):
         base64_part = authorization_header.split(" ")[1].strip()
 
         return base64_part
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        '''
+        Function that returns the decoded value of a Base64
+        string base64_authorization_header
+        '''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
