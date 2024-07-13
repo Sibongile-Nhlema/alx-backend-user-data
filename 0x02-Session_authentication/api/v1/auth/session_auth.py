87622beba64b6c947121e0c1f464a76949b7e885
Sibongile <sibongile.nhlema@gmail.com>
2024-07-13 19:59:00 +0200

    created class attribute user_id_by_session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,29 @@
 Module for the implementaion of SessionAuth
 '''
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''
     Class for the SessionAuth implementation
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        Creates a Session ID for a user_id.
+
+        Args:
+            user_id (str): ID of the user for whom the session is created.
+
+        Returns:
+            str: generated Session ID or None
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id         
