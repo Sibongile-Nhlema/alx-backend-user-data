0f10341e92b6e8f4385452bb73eff0174bca2d36
Sibongile <sibongile.nhlema@gmail.com>
2024-07-13 22:35:12 +0200

    implemented current_user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Module for the implementaion of SessionAuth
 '''
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -42,3 +43,17 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''
+        Returns a User instance based on a cookie value
+        '''
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        
+        return User.get(user_id)
