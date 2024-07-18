64beda5e290cbebeea4ab0f838a6cf20d8a3fc28
Sibongile <sibongile.nhlema@gmail.com>
2024-07-18 21:33:54 +0200

    implement the end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,11 +2,11 @@
 '''
 Basic Flask app
 '''
-
-from flask import Flask, jsonify
-
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -15,6 +15,22 @@ def welcome():
     GET route that returns a JSON payload
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    '''
+    POST route to register a user
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 
 if __name__ == "__main__":
